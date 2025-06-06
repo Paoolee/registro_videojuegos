5c3551ee73cf54de7dd36bf67c8e5ae399fac8a2
Paoolee <pao.ordenes@duocuc.cl>
2025-06-06 10:44:28 -0400

    Pso 1: Estructura del menú y tupla de plataformas creada

---
+++ videojuegos.py
@@ -0,0 +1,30 @@
+videosjuegos = []
+
+plataformas = ("PC", "PSS","xbox series X","Nintendo Switch")
+
+while True:
+    print("\n-- MENÚ DE VIDEOJUEGOS---")
+    print("1. Registrar videojuego")
+    print("2. Ver videojuego")
+    print("3. Modificar videojuego")
+    print("3.Eliminar videojuego")
+    print("4.Salir ")
+
+    opcion = input("Seleccione una opción (1-5):")
+
+    if opcion =="1":
+        pass
+    elif opcion == "2":
+        pass
+    elif opcion == "3":
+     pass
+    elif opcion == "4":
+       pass
+    elif opcion == "5":
+       print("Saliendo del programa.")
+       break
+    else:
+       print("Opción invalida")
+           
+
+    
